23381e3ba1d5e80579da3b4117b6fa14a095e658
Shimeta <kahilrazor@gmail.com>
2018-05-12 19:31:40 +0200

    Update Zebra.java

--- ZooProject/src/zoo/Tier/Zebra.java
+++ ZooProject/src/zoo/Tier/Zebra.java
@@ -1,14 +1,10 @@
 package zoo.Tier;
-
 import zoo.Zoo;
-//import zoo.Gehege.Gehege;
 
 public class Zebra extends LandsaeugeTier{
-
 	public Zebra(String name, Zoo zoo) {
 		super(name, zoo);
 		this.TypName="Zebra";
 		System.out.println("Das Zebra "+ name+ " wurde erstellt");
 	}
-
 }
