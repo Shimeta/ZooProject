07e9bbdbc48abc577a99da28b56fb14c4588de4a
Shimeta <kahilrazor@gmail.com>
2018-05-12 19:26:54 +0200

    Update Vogel.java

--- ZooProject/src/zoo/Tier/Vogel.java
+++ ZooProject/src/zoo/Tier/Vogel.java
@@ -1,13 +1,9 @@
 package zoo.Tier;
-
 import zoo.Zoo;
-//import zoo.Gehege.Gehege;
 
 public abstract class Vogel extends Tier{
-	
 	public Vogel(String name, Zoo zoo){
 		super(name, zoo);
 		this.Gehege_typ = 1;
 	}
-	
 }
