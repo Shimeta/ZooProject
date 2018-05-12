de12a2f0bf83840ac81437e0437d871d44909e04
Shimeta <kahilrazor@gmail.com>
2018-05-12 19:15:52 +0200

    Update Personen.java

--- ZooProject/src/zoo/Personen/Personen.java
+++ ZooProject/src/zoo/Personen/Personen.java
@@ -1,38 +1,25 @@
 package zoo.Personen;
 
 import zoo.Gehege.Gehege;
-//import zoo.Tier.Tier;
-//import zoo.Tier.ITier; not used yet
+
 
 public abstract class Personen {
 	
-	private String name;
-	private Gehege[] gehege; //TODO: Personen betreuen 	ist hier nicht erwünscht
-							 // diese variable ist für den Ort der Person da (UML)
-	
+	private String name;	
 	
 	public Personen(String name, Gehege[] gehege) {
-		this.setName(name);
+	this.setName(name);
+	
 	}
 
-
-	///////name
 	public String getName() {
 		return name;
 	}
+
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public Gehege[] getGehege() {
-		return gehege;
-	}
 
-	public void setGehege(Gehege[] gehege) {
-		this.gehege = gehege;
-	}
-	
-	
-	
 }
