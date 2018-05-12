8165529e3003d758c32b7b81e2ff8aae00870bb0
Shimeta <kahilrazor@gmail.com>
2018-05-12 19:24:46 +0200

    Update LandsaeugeTier.java

--- ZooProject/src/zoo/Tier/LandsaeugeTier.java
+++ ZooProject/src/zoo/Tier/LandsaeugeTier.java
@@ -1,12 +1,9 @@
 package zoo.Tier;
-
 import zoo.Zoo;
-//import zoo.Gehege.Gehege;
 
 public abstract class LandsaeugeTier extends Tier {
-
 	public LandsaeugeTier(String name, Zoo zoo) {
 		super(name, zoo);
 		this.Gehege_typ = 2;
-		}
+	}
 }
