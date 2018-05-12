66dd814b0e7dbe694a937bf6eb1245b5ab3bfb11
Shimeta <kahilrazor@gmail.com>
2018-05-12 19:23:23 +0200

    Update KugelFisch.java

--- ZooProject/src/zoo/Tier/KugelFisch.java
+++ ZooProject/src/zoo/Tier/KugelFisch.java
@@ -1,14 +1,10 @@
 package zoo.Tier;
-
 import zoo.Zoo;
-//import zoo.Gehege.Gehege;
 
 public class KugelFisch extends WasserTier{
-
 	public KugelFisch(String name, Zoo zoo) {
 		super(name, zoo);
 		this.TypName="Kugelfisch";
 		System.out.println("Der Kugelfisch "+name+ " wurde erstellt");
-		
 	}
 }
